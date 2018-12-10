c18d8e880bc61f4ee8ad8ca3257152fbd22efd0a
horis233 <horis233@gmail.com>
2018-12-09 23:04:52 -0500

    change navbar upon sign up

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,52 @@
 import React from "react";
-import { Box, Text, Heading, Image } from "gestalt";
+import { Box, Text, Heading, Image, Button } from "gestalt";
+import { getToken } from "../utils";
 import { NavLink } from "react-router-dom";
 
-const Navbar = () => (
+const Navbar = () => {
+  return getToken() !== null ? <AuthNav /> : <UnAuthNav />;
+};
+
+const AuthNav = () => (
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="around"
+    height={70}
+    color="midnight"
+    padding={1}
+    shape="roundedBottom"
+  >
+    {/* Checkout Link */}
+    <NavLink activeClassName="active" to="/checkout">
+      <Text size="xl" color="white">
+        Checkout
+      </Text>
+    </NavLink>
+
+    {/* Title and Logo */}
+    <NavLink activeClassName="active" exact to="/">
+      <Box display="flex" alignItems="center">
+        <Box margin={2} height={50} width={50}>
+          <Image
+            alt="BrewHaha Logo"
+            naturalHeight={1}
+            naturalWidth={1}
+            src="./icons/logo.svg"
+          />
+        </Box>
+        <Heading size="xs" color="orange">
+          BrewHaha
+        </Heading>
+      </Box>
+    </NavLink>
+
+    {/* Signout Button */}
+    <Button color="transparent" text="Sign Out" inline size="md" />
+  </Box>
+);
+
+const UnAuthNav = () => (
   <Box
     display="flex"
     alignItems="center"
@@ -13,14 +57,14 @@ const Navbar = () => (
     shape="roundedBottom"
   >
     {/* Sign In Link */}
-    <NavLink to="/signin">
+    <NavLink activeClassName="active" to="/signin">
       <Text size="xl" color="white">
         Sign In
       </Text>
     </NavLink>
 
     {/* Title and Logo */}
-    <NavLink to="/">
+    <NavLink activeClassName="active" exact to="/">
       <Box display="flex" alignItems="center">
         <Box margin={2} height={50} width={50}>
           <Image
